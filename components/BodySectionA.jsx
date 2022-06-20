6e5283eb8e793bb73a3996cfbede22542b1d4a26
Chrisdocs <chriswolfe.dev@gmail.com>
2022-06-20 17:02:06 -0500

    trying to solve title char > 8 issue

--- components/BodySectionA.jsx
+++ components/BodySectionA.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../styles/Home.module.scss';
 
-export default function BodySectionA () {
+export default function BodySectionA (style1, style2, title) {
 	return (
 		<div className={styles.bodyA}>
 			<h3>TITLE</h3>
