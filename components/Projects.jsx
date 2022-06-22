6a9c7a24b8357a9b85f0733bdc8bac557f6fc588
Chrisdocs <chriswolfe.dev@gmail.com>
2022-06-21 19:44:40 -0500

    created projects component

---
+++ components/Projects.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/Home.module.scss';
+
+export default function Projects () {
+	return(
+		<div className={styles.projects} id='projects'>
+
+		</div>
+	)
+}
